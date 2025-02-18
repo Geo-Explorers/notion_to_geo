2be09efd5700e62bc820337d5845652025e95834
Byron Guina <byron.guina@gmail.com>
2025-02-18 09:26:15 -0800

    feat: publish wrapper function

--- main.ts
+++ main.ts
@@ -1,6 +1,7 @@
 import { IPFS, Relation, Triple } from "@graphprotocol/grc-20";
 import { deploySpace } from "./src/deploy-space";
 import { wallet } from "./src/wallet";
+import { publish } from "./src/publish";
 
 async function main() {
 	// If you haven't deployed a personal space yet you can deploy one
@@ -41,23 +42,10 @@ async function main() {
 		ops: [newTriple, newRelation],
 	});
 
-	// This returns the correct contract address and calldata depending on the space id
-	// Make sure you use the correct space id in the URL below and the correct network.
-	const result = await fetch(`https://api-testnet.grc-20.thegraph.com/space/${spaceId}/edit/calldata`, {
-		method: "POST",
-		body: JSON.stringify({
-			cid: cid,
-			network: "TESTNET",
-		}),
-	});
-
-	const { to, data } = await result.json();
-
 	// Write to the smart contract using the above calldata. This returns a transaction hash.
-	const txResult = await wallet.sendTransaction({
-		to: to,
-		value: 0n,
-		data: data,
+	const txHash = await publish({
+		spaceId,
+		cid,
 	});
 
 	// If you've done these steps correctly then your data should be published to your personal space!

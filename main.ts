f3758cc66d645b904b2295635ef22b33a7223657
Byron Guina <byron.guina@gmail.com>
2025-02-18 09:05:44 -0800

    feat: fix formatting

--- main.ts
+++ main.ts
@@ -1,66 +1,67 @@
-import { IPFS, Relation, Triple } from '@graphprotocol/grc-20';
-import { deploySpace } from './src/deploy-space';
-import { wallet } from './src/wallet';
+import { IPFS, Relation, Triple } from "@graphprotocol/grc-20";
+import { deploySpace } from "./src/deploy-space";
+import { wallet } from "./src/wallet";
 
 async function main() {
-  // If you haven't deployed a personal space yet you can deploy one
-  // by running deploySpace. This will return the spaceId. Make sure
-  // you remember this.
-  //
-  // If you've already deployed a personal space and have the spaceId
-  // you can skip this step.
-  const spaceId = await deploySpace({
-    spaceName: 'YOUR SPACE NAME',
-    initialEditorAddress: 'YOUR WALLET ACCOUNT ADDRESS' // 0x...
-  })
+	// If you haven't deployed a personal space yet you can deploy one
+	// by running deploySpace. This will return the spaceId. Make sure
+	// you remember this.
+	//
+	// If you've already deployed a personal space and have the spaceId
+	// you can skip this step.
+	const spaceId = await deploySpace({
+		spaceName: "YOUR SPACE NAME",
+		initialEditorAddress: "YOUR WALLET ACCOUNT ADDRESS", // 0x...
+	});
 
-  // Once you have a personal space you can write data to it. Generate
-  // ops for your data using Triple.make or Relation.make accordingly.
-  const newTriple = Triple.make({
-    attributeId: '...', 
-    entityId: '...',
-    value: {
-      type: 'TEXT',
-      value: ''
-    }
-  })
+	console.log("Your spaceId is:", spaceId);
 
-  const newRelation = Relation.make({
-    fromId: '...',
-    toId: '...',
-    relationTypeId: '...'
-  })
+	// Once you have a personal space you can write data to it. Generate
+	// ops for your data using Triple.make or Relation.make accordingly.
+	const newTriple = Triple.make({
+		attributeId: "...",
+		entityId: "...",
+		value: {
+			type: "TEXT",
+			value: "",
+		},
+	});
 
-  // Publish your data to IPFS. Give the edit an appropriate name and pass it
-  // any ops you want to publish together.
-  const cid = await IPFS.publishEdit({
-    name: 'YOUR EDIT NAME',
-    author: 'YOUR WALLET ACCOUNT ADDRESS',
-    ops: [newTriple, newRelation]
-  })
+	const newRelation = Relation.make({
+		fromId: "...",
+		toId: "...",
+		relationTypeId: "...",
+	});
 
+	// Publish your data to IPFS. Give the edit an appropriate name and pass it
+	// any ops you want to publish together.
+	const cid = await IPFS.publishEdit({
+		name: "YOUR EDIT NAME",
+		author: "YOUR WALLET ACCOUNT ADDRESS",
+		ops: [newTriple, newRelation],
+	});
 
-  // This returns the correct contract address and calldata depending on the space id
-  // Make sure you use the correct space id in the URL below and the correct network.
-  const result = await fetch(`https://api-testnet.grc-20.thegraph.com/space/${spaceId}/edit/calldata`, {
-    method: "POST",
-    body: JSON.stringify({ 
-      cid: cid,
-      network: "TESTNET",
-    }),
-  });
+	// This returns the correct contract address and calldata depending on the space id
+	// Make sure you use the correct space id in the URL below and the correct network.
+	const result = await fetch(`https://api-testnet.grc-20.thegraph.com/space/${spaceId}/edit/calldata`, {
+		method: "POST",
+		body: JSON.stringify({
+			cid: cid,
+			network: "TESTNET",
+		}),
+	});
 
-  const { to, data } = await result.json();
+	const { to, data } = await result.json();
 
-  // Write to the smart contract using the above calldata. This returns a transaction hash.
-  const txResult = await wallet.sendTransaction({
-    to: to,
-    value: 0n,
-    data: data,
-  });
+	// Write to the smart contract using the above calldata. This returns a transaction hash.
+	const txResult = await wallet.sendTransaction({
+		to: to,
+		value: 0n,
+		data: data,
+	});
 
-  // If you've done these steps correctly then your data should be published to your personal space!
-  // Check it out at the testnet explorer URL
+	// If you've done these steps correctly then your data should be published to your personal space!
+	// Check it out at the testnet explorer URL
 }
 
-main()
+main();
--- src/deploy-space.ts
+++ src/deploy-space.ts
@@ -1,4 +1,4 @@
-import { getChecksumAddress } from '@graphprotocol/grc-20';
+import { getChecksumAddress } from "@graphprotocol/grc-20";
 
 type DeploySpaceOptions = {
 	initialEditorAddress: string;
@@ -6,14 +6,14 @@ type DeploySpaceOptions = {
 };
 
 export async function deploySpace(options: DeploySpaceOptions) {
-  const result = await fetch("https://api-testnet.grc-20.thegraph.com/deploy", {
-    method: "POST",
-    body: JSON.stringify({
-      initialEditorAddress: getChecksumAddress(options.initialEditorAddress),
-      spaceName: options.spaceName,
-    }),
-  });
-  
-  const {spaceId} = await result.json();
-  console.log("SPACE ID", spaceId);
+	const result = await fetch("https://api-testnet.grc-20.thegraph.com/deploy", {
+		method: "POST",
+		body: JSON.stringify({
+			initialEditorAddress: getChecksumAddress(options.initialEditorAddress),
+			spaceName: options.spaceName,
+		}),
+	});
+
+	const { spaceId } = await result.json();
+	return spaceId;
 }
